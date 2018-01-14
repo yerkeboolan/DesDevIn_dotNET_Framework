d2e32b1901e731892c29b56c6e0569d7fff90080
Yerkebulan <erke.zhumagali@gmail.com>
2018-01-15 00:22:25 +0600

    Lab 1[3] added

---
+++ Lab 1/Lab 1[3]/ViewTextFiles/ViewTextFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ViewTextFiles
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader tr = new StreamReader(@"C:\user\wow.txt");
+                try
+                {
+                    Console.WriteLine(tr);
+                }
+                catch (Exception u)
+                {
+                    Console.WriteLine(u);
+                }
+                tr.Close();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Sorry, you lack sufficient privileges.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Sorry, the file does not exist.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
