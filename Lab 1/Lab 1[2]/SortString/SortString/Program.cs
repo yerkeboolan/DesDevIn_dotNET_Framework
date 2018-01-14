c9455c2a676b9a4822eefa6db2345d37724ec368
Yerkebulan <erke.zhumagali@gmail.com>
2018-01-14 19:59:26 +0600

    Lab 1[2] added

---
+++ Lab 1/Lab 1[2]/SortString/SortString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "If you can do it";
+            string[] words = sentence.Split(' ');
+
+            string str = string.Join(" ", words);
+            Console.WriteLine(str);
+
+            //Array.Sort(words);
+            /*for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i].ToString());
+            }
+            */
+            /* foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            } */
+            Console.ReadKey();
+        }
+    }
+}
