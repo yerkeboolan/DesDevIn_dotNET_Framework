762a62b8e1c00a7c1ce362fe0d2f07f920e32c6d
Yerkebulan <erke.zhumagali@gmail.com>
2018-01-14 20:41:01 +0600

    Lab 1[4] added

---
+++ Lab 1/Lab 1[4]/Manager/Manager/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class Manager: Person
+    {
+        public string phoneNumber, officeLocation;
+
+        public Manager(string firstName, string lastName, int age, Genders gender, string phoneNumber, string officeLocation) : base(firstName, lastName, age, gender)
+        {
+            this.phoneNumber = phoneNumber;
+            this.officeLocation = officeLocation;
+        }
+
+        public override string ToString()
+        {
+            return "First name: " + firstName + "\n" + "Last name: " + lastName + "\n" + "Age: " + age + "\n" + "Gender: " + gender + "\n" + "Phone number: " + phoneNumber + "\n" + "Office location: " + officeLocation;
+
+        }
+    }
+}
---
+++ Lab 1/Lab 1[4]/Manager/Manager/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    enum Genders {Male, Female};
+    class Person
+    {
+        public string firstName, lastName;
+        public int age;
+        public Genders gender;
+
+        public Person(string firstName, string lastName, int age, Genders gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public Person(Genders gender)
+        {
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return "First name: " + firstName + "\n" + "Last name: " + lastName + "\n" + "Age: " + age + "\n" + "Gender: " + gender;
+        }
+
+    }
+}
