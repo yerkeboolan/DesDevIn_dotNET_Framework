f0fd24a4e9bc9930e63f6ed46d3d9c80adb7ed5f
Yerkebulan <erke.zhumagali@gmail.com>
2018-01-31 00:03:50 +0600

    FullSpellCorrector

--- [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.cs
+++ [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FullSpellCorrector
 {
+    [Serializable]
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,6 +23,8 @@ namespace FullSpellCorrector
             button4.Visible = true;
         }
 
+       
+        static List<String> words = null;
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if(checkBox1.Checked == true)
@@ -42,7 +48,65 @@ namespace FullSpellCorrector
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+            string text = " ";
+            string word = " ";
+            if (text == richTextBox1.Text) return;
+            text = richTextBox1.Text;
+            int n = text.Length;
+            if(!Char.IsLetter(text[n-1]))
+            {
+                for(int i = n - 2; i >= 0; i--)
+                {
+                    if(i == 0) { word = text.Substring(i, n - i - 1); break; }
+                    if(!Char.IsLetter(text[i])) { word = text.Substring(i + 1, n - i - 2); break; }
+                }
+            }
+        }
 
+
+
+
+
+
+        static void Serialize()
+        {
+            FileStream fs = new FileStream("words.dat", FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            try
+            {
+                bf.Serialize(fs, words);
+            }
+            catch(SerializationException se)
+            {
+                System.Diagnostics.Debug.Write(se.Message);
+                throw;
+            }
+
+            finally
+            {
+                fs.Close();
+            }
+        }
+        static void Deserialize()
+        {
+            FileStream fs = new FileStream("words.dat", FileMode.Open);
+            try
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+
+                words = (List<string>)bf.Deserialize(fs);
+            }
+            catch (SerializationException se)
+            {
+                System.Diagnostics.Debug.Write(se.Message);
+                throw;
+            }
+
+            finally
+            {
+                fs.Close();
+            }
         }
     }
 }
