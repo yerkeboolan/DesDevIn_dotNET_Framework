bacf230b2989ab1b076e5f14f7ce7049f96ce5c2
Yerkebulan <erke.zhumagali@gmail.com>
2018-01-31 22:11:56 +0600

    SpellCorrector updating...

--- [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.Designer.cs
+++ [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -46,6 +47,7 @@
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseDoubleClick);
             // 
             // checkBox1
             // 
@@ -69,6 +71,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "+ Add";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -81,22 +84,12 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Ignore";
             this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.SystemColors.Highlight;
-            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(430, 171);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(109, 64);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Ignore All";
-            this.button3.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(430, 379);
+            this.button4.Location = new System.Drawing.Point(430, 406);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(109, 45);
             this.button4.TabIndex = 5;
@@ -114,6 +107,12 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Not in dictionary:";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 26);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -121,7 +120,6 @@
             this.ClientSize = new System.Drawing.Size(565, 463);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
@@ -139,9 +137,9 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.cs
+++ [Lab 2] FullSpellCorrector/FullSpellCorrector/FullSpellCorrector/Form1.cs
@@ -23,21 +23,52 @@ namespace FullSpellCorrector
             button4.Visible = true;
         }
 
-       
-        static List<String> words = null;
+
+        static List<string> words = null;
+        static List<string> answers = new List<string>();
+        static List<string> temps_w = new List<string>();
+        static List<string> temps_c = new List<string>();
+
+        int mini, position = 0;
+        int[,] pd = null;
+        bool ok = false;
+        char ch = '$';
+        string signs = "$.!?";
+        string selected = "";
+
+
+
+
+
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if(checkBox1.Checked == true)
             {
                 button1.Visible = false;
                 button2.Visible = false;
-                button3.Visible = false;    
+                string text = richTextBox1.Text;
+                if (temps_c.Count > 0)
+                {
+                    for (int i = 0; i < temps_c.Count; i++)
+                    {
+                        text = text.Replace(temps_w[i], temps_c[i]);
+                        System.Diagnostics.Debug.Write(temps_w[i]);
+                    }
+                }
+
+                richTextBox1.Text = text;
+                richTextBox1.SelectAll();
+                richTextBox1.SelectionColor = Color.Black;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.SelectionLength = 0;
+                temps_c.Clear();
+                temps_w.Clear();
             }
-                else
+
+            else
             {
                 button1.Visible = true;
                 button2.Visible = true;
-                button3.Visible = true;
             }
         }
 
@@ -46,67 +77,292 @@ namespace FullSpellCorrector
             this.Close();
         }
 
+
+
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            string text = " ";
-            string word = " ";
+            string text = "";
+            string word = "";
+            int lol = 0, n;
+            if (richTextBox1.Text.Length > position) position = richTextBox1.Text.Length;
+            else
+            {
+                position = richTextBox1.Text.Length + 1;
+            }
             if (text == richTextBox1.Text) return;
             text = richTextBox1.Text;
-            int n = text.Length;
-            if(!Char.IsLetter(text[n-1]))
+
+            n = richTextBox1.SelectionStart;
+
+            richTextBox1.SelectionColor = Color.Black;
+
+            System.Diagnostics.Debug.WriteLine(richTextBox1.SelectionStart);
+
+
+            if (n - 1 > 0 && text != "" && !Char.IsLetter(text[n - 1]) && !char.IsDigit(text[n - 1]))
             {
-                for(int i = n - 2; i >= 0; i--)
+
+                for (int i = n - 2; i >= 0; i--)
                 {
-                    if(i == 0) { word = text.Substring(i, n - i - 1); break; }
-                    if(!Char.IsLetter(text[i])) { word = text.Substring(i + 1, n - i - 2); break; }
+
+                    if (i == 0)
+                    {
+                        word = text.Substring(i, n - i - 1);
+
+                        break;
+
+                    }
+                    if (!Char.IsLetter(text[i]) && !char.IsDigit(text[i]))
+                    {
+                        word = text.Substring(i + 1, n - i - 2);
+
+                        lol = i;
+                        break;
+                    }
+
+                }
+                if (word == "") return;
+                text = text.Replace(word, word.ToLower());
+
+                word = word.ToLower();
+
+                if (words.Contains(word) || char.IsNumber(word[0]))
+                {
+                    
+                    //richTextBox1.Text = text;
+                    richTextBox1.SelectionStart = n;
+                    return;
+
+                }
+                else
+                {
+
+                    if (checkBox1.Checked)
+                    {
+                        c_Checked(text, word, n, lol);
+
+                    }
+                    else
+                    {
+
+                        c_NotChecked(word, n);
+
+
+                    }
+                }
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+        public void c_Checked(string text, string word, int n, int lol)
+        {
+            string ans = FindTheMinEditDistance(word);
+
+            for (int i = 0; i < signs.Length; i++)
+            {
+                if (lol - 1 > 0 && signs[i] == text[lol - 1]) ok = true;
+            }
+            if (ok && char.IsLetter(ans[0]) || lol == 0)
+            {
+                ans = FirstToUpperCase(ans);
+                ok = false;
+            }
+
+            text = text.Replace(word, ans);
+           // FindBackTrace(word, ans);
+            if (text[text.Length - 1] != 32) text += " ";
+            else if (lol == 0 || text[lol] == 32 && !char.IsLetter(text[lol - 1])) text += " ";
+            richTextBox1.Text = text;
+            if (text[n - word.Length + ans.Length - 1] != 32) richTextBox1.SelectionStart = n - word.Length + ans.Length + 1;
+            else richTextBox1.SelectionStart = n - word.Length + ans.Length;
+        }
+
+
+
+
+
+
+        public string FindTheMinEditDistance(string word)
+        {
+            string s_comp, ans = "";
+            mini = 1000;
+            int[,] dp = null;
+
+            for (int k = 0; k < words.Count; k++)
+            {
+                s_comp = words[k];
+                dp = new int[word.Length + 1, s_comp.Length + 1];
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    dp[i, 0] = i;
+                }
+                for (int j = 0; j <= s_comp.Length; j++)
+                {
+                    dp[0, j] = j;
+                }
+
+                for (int i = 1; i <= word.Length; i++)
+                {
+                    for (int j = 1; j <= s_comp.Length; j++)
+                    {
+                        if (word[i - 1] == s_comp[j - 1])
+                        {
+                            dp[i, j] = Math.Min(Math.Min(dp[i - 1, j] + 1, dp[i, j - 1] + 1), dp[i - 1, j - 1]);
+                        }
+                        else
+                        {
+                            dp[i, j] = Math.Min(Math.Min(dp[i - 1, j] + 1, dp[i, j - 1] + 1), dp[i - 1, j - 1] + 2);
+                        }
+                    }
+                }
+
+                if (dp[word.Length, s_comp.Length] <= mini)
+                {
+                    mini = dp[word.Length, s_comp.Length];
+                    ans = s_comp;
+                    pd = dp;
+                    if (mini <= 5)
+                    {
+                        answers.Add(ans);
+                    }
+                }
+
+
+
+
+            }
+
+
+            return ans;
+        }
+
+
+
+        public string FirstToUpperCase(string s)
+        {
+            char c = char.ToUpper(s[0]);
+            string ans = "";
+            ans += c;
+            for (int j = 1; j < s.Length; j++)
+            {
+                ans += s[j];
+            }
+
+            return ans;
+        }
+
+        public string ReplaceWord(string text, string word, string ans, int n)
+        {
+            int i = n;
+            string s = text.Remove(n, word.Length);
+
+            string k = s.Insert(n, ans);
+            return k;
+        }
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            words.Add(selected);
+
+
+            //System.Diagnostics.Debug.Write(richTextBox1.SelectedText);
+            richTextBox1.SelectionColor = Color.Black;
+            selected = "";
+            words.Sort();
+        }
+
+        private void richTextBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.AutoWordSelection = true;
+            string word = richTextBox1.SelectedText;
+            selected = word;
+            if (word.Contains(" ")) word.Remove(word.Length - 1, 1);
+            contextMenuStrip1.Items.Clear();
+            // System.Diagnostics.Debug.WriteLine(word);
+            int n = richTextBox1.SelectionStart + word.Length;
+            int lol = richTextBox1.SelectionStart;
+            string text = richTextBox1.Text;
+            /* if (checkBox1.Checked) c_Checked(text, word, n, lol);
+             else c_NotChecked(word, n);
+             */
+            string lg = FindTheMinEditDistance(word);
+            if (answers.Count > 0)
+            {
+                for (int i = 0; i < answers.Count; i++)
+                {
+                    contextMenuStrip1.Items.Add(answers[i]);
                 }
             }
+            if (contextMenuStrip1.Items.Count == 0) contextMenuStrip1.Items.Add(lg);
+            contextMenuStrip1.Show(Cursor.Position);
+            answers.Clear();
+            // contextMenuStrip1.Items.Add(word);
+
+
         }
 
-
-
-
-
-
-        static void Serialize()
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            FileStream fs = new FileStream("words.dat", FileMode.Create);
+            string clicked = e.ClickedItem.Text;
 
-            BinaryFormatter bf = new BinaryFormatter();
-            try
-            {
-                bf.Serialize(fs, words);
-            }
-            catch(SerializationException se)
-            {
-                System.Diagnostics.Debug.Write(se.Message);
-                throw;
-            }
+            contextMenuStrip1.Visible = true;
+            string text = richTextBox1.Text;
+            int n = richTextBox1.SelectionStart;
 
-            finally
-            {
-                fs.Close();
-            }
+            //char ch = text[n + richTextBox1.SelectionLength];
+            //clicked += ch;
+            string word = richTextBox1.SelectedText.Remove(richTextBox1.SelectionLength - 1, 1);
+
+            text = ReplaceWord(text, word, clicked, n);
+            richTextBox1.Text = text;
+            richTextBox1.Select(n, clicked.Length);
+            richTextBox1.SelectionColor = Color.Black;
+            richTextBox1.SelectionStart = n + clicked.Length;
+
+            System.Diagnostics.Debug.Write(clicked);
         }
-        static void Deserialize()
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream("words.dat", FileMode.Open);
-            try
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-
-                words = (List<string>)bf.Deserialize(fs);
-            }
-            catch (SerializationException se)
-            {
-                System.Diagnostics.Debug.Write(se.Message);
-                throw;
-            }
-
-            finally
-            {
-                fs.Close();
-            }
+            richTextBox1.SelectionColor = Color.Black;
         }
+
+
+
+
+
+        public void c_NotChecked(string word, int n)
+        {
+
+            temps_w.Add(word);
+            temps_c.Add(FindTheMinEditDistance(word));
+            richTextBox1.SelectionStart = n - word.Length - 1;
+            richTextBox1.SelectionLength = word.Length;
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.SelectionStart = n;
+            richTextBox1.SelectionLength = 0;
+
+        }
+
     }
 }
